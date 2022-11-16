b59816b411c9d5d62ef8b458205be223a96ca22a
Sajson <amokrzycki96@gmail.com>
2022-11-16 16:51:27 +0100

    Template of React Chrome Extension

---
+++ src/popup/popup.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "react-dom";
+
+function Popup() {
+  return (
+    <div>
+      <div>Example of popup</div>
+    </div>
+  );
+}
+
+render(<Popup />, document.getElementById("root"));
